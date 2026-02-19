4719ca35d66ee4178a48ab607e5afb0d32310e2c
Marcos santos <Jaaziel>
2026-02-18 22:25:03 -0300

    Criando classe de order e item

---
+++ Data/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace enexlabs
+
+public class Order
+{
+	{
+
+      public int Id { get; set; }
+      public User Userid { get; set; }
+      public string OrderData { get; set; }
+      public string Status { get; set; }
+      public string Totalvalue { get; set; }
+      public string Pagamentform { get; set; }
+      public string DeliveryAddress { get; set; }
+
+    }
+}
---
+++ Data/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace enexlabs 
+
+public class OrderItem
+{
+	{
+       public string Id { get; set; }
+	   public OrderItem Order { get; set; }
+	   public OrderItem OrderId { get; set; }
+       public string ProductId { get; set; }
+	   public string Quantity { get; set; }
+	   public string UnitPrice { get; set; }
+       public string UnitPriceUnit { get; set; }
+	   public string SubTotal { get; set; }
+	}
+}
